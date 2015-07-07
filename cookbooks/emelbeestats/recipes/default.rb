c2beef450cb34f244c0d12c816e6a6f65c93a104
codemunkee@gmail.com <codemunkee@gmail.com>
2015-07-07 17:55:36 +0000

    replaced sendmail with postfix

--- cookbooks/emelbeestats/recipes/default.rb
+++ cookbooks/emelbeestats/recipes/default.rb
@@ -16,6 +16,20 @@ yum_package 'screen' do
   action :install
 end
 
+yum_package 'sendmail' do
+  action :remove
+end
+
+yum_package 'postfix' do
+  action :install
+end
+
+service 'postfix' do
+  supports :status => true, :reload => true, :restart => true
+  action :start
+  subscribes :run, 'yum_package[sendmail]', :immediately
+end
+
 python_pip 'flask'
 python_pip 'twilio'
 python_pip 'requests'
